a995a6d019d7294efda9ed2a9d858e65c9675fa3
Vinod Dosapati <dpativinod@gmail.com>
2019-09-16 23:55:27 -0400

    Add files via upload

---
+++ testproj/src/main/java/com/firstsofteng/testproj/TestprojApplication.java
@@ -0,0 +1,13 @@
+package com.firstsofteng.testproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprojApplication.class, args);
+	}
+
+}
---
+++ testproj/src/main/java/com/firstsofteng/testproj/Welcome.java
@@ -0,0 +1,12 @@
+package com.firstsofteng.testproj;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+	@RequestMapping("/")
+	public String firstmethod() {
+		return "Hello World!!";
+	}
+}
